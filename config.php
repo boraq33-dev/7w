30a8b745fbeee19ba960084c0cd94bf9e1a28ef6
boraq33-dev <boraq33@gmail.com>
2026-09-12 13:50:15 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 return [
-    'bot_token' => '8720763052:AAFDbjT7QbsNa9JKmKhRQicFrduaMbaJg',
+    'bot_token' => '8720763052:AAFDbjT7QbsMa9JKJ7mKhRQicFrduaMbaJg',
     'chat_id' => '8738732401'
 ];
